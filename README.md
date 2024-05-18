781f9b1b6d5634b42010ef63d323e8bec7053330
DX <dxcode01@gmail.com>
2024-05-18 16:23:59 -0500

    form-validation after submit : done input-validation after user change them : done code-documented at readme : done

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-testingInput(input) :
-test if imput is valid , style valid ones with .valid , invalid with .invalid , if inp valid , style small in parent div with pre-added hide class , invalid with pre added show class
+APP : core object
+
+
+setListeners : select form , form's inputs add each event listener and switch default browser validation to false.
+
+testingInput : check if input's value is invalid or valid , applay .valid or .invalid css's classes to them relaing on switchValidityStyle()
+
+switchValidityStyle : switch between .valid or .invalid css's classes
+switchValErrorMsg : switch between a hideClassName to showClassName for error msg , it was designed for small tag below each input.
--- app.js
+++ app.js
@@ -1,23 +1,82 @@
-// validation after submition 
-// validate after change 
 
+const APP = {
+    setListeners : function(){
+        let form = document.querySelector('form');
+        form.noValidate = true;
 
+        let email = document.querySelector('#email');
+        let password = document.querySelector('#password');
+        let Cpass = document.querySelector('#cPassword');
+        
+        password.addEventListener('blur' , e=>{
+            this.testingInput(password);
+            this.confirmPass(password , Cpass);
+        });
 
+        Cpass.addEventListener('blur' , e=>{
+            this.confirmPass(password , Cpass);
+        });
 
-//after change testingInput 
-//if input is correct style valid  
-//if not display a mssg and style invalid
+        email.addEventListener('blur' , e=>{
+            this.testingInput(email);
+        });
+        
+        form.addEventListener('submit' , eve =>{
+            eve.preventDefault(); //stop submition
+            //valid
+            if(form.checkValidity()){
+                if(this.confirmPass(password , Cpass)){
+                    form.submit();
+                }
+            }
+            //invalid 
+            else {
+                this.testingInput(password);
+                this.testingInput(email);
+            }
+        });
+    } ,
 
-/*
-    parent must be a div with small at the end.
-    small must have two class , one hide other unhide
-    test input must have two class , valid & invalid
-    style input & small baed on validity 
-
-*/
-function testingInput(input){
-    //switch between css class valid & invalid for input
-    function switchValidityStyle(input , status = 'valid' ){
+    testingInput : function(input){
+        //valid input value
+        if(input.checkValidity() == true){
+            //style input
+           this.switchValidityStyle(input , "valid"); 
+           //style error msg input
+           let errorMsg = input.parentElement.lastElementChild;
+           this.switchValErrorMSg(errorMsg , "error-msg-hide" , "error-msg-show" , 'valid');
+           
+        } 
+        //invalid input's value 
+        else {
+            //style input
+            this.switchValidityStyle(input , "invalid");
+            //style error msg field
+            let errorMsg = input.parentElement.lastElementChild;
+            this.switchValErrorMSg(errorMsg , 'error-msg-hide' , "error-msg-show" , 'invalid');
+    
+        }
+    },
+    confirmPass : function(pass , cPass){
+        if(cPass.value != '' || pass.value != ""){
+            if(pass.value == cPass.value){
+                //same
+                this.switchValidityStyle(cPass , 'valid');
+                let errorMsg = cPass.parentElement.lastElementChild;
+                this.switchValErrorMSg(errorMsg , 'error-msg-hide' , "error-msg-show" , 'valid');
+                return true ;
+        
+                
+            } else {
+                // different
+                this.switchValidityStyle(cPass , 'invalid')
+                let errorMsg = cPass.parentElement.lastElementChild;
+                this.switchValErrorMSg(errorMsg , 'error-msg-hide' , "error-msg-show" , 'invalid');
+                return false ;
+            }
+        }
+    },
+    switchValidityStyle : function(input , status = 'valid' ){
             //was selected before 
         if(input.classList.contains("invalid") || input.classList.contains('valid')){
             if(status == 'valid'){
@@ -34,13 +93,12 @@ function testingInput(input){
                 }
             }
         } 
-           //no select before
+        //no select before
         else {
             input.classList.add(status);
         }
-    }
-    //switch between hide and show css class for small tag > error msg
-    function switchValErrorMSg(tag , hideClassName , showClassName , validity){
+    },
+    switchValErrorMSg : function(tag , hideClassName , showClassName , validity){
         if(validity == 'valid'){
             //valid
                 if(tag.classList.contains(hideClassName) || tag.classList.contains(showClassName)){
@@ -51,7 +109,7 @@ function testingInput(input){
                 } else {
                     tag.classList.add(hideClassName);
                 }
-           
+        
         }else if (validity == 'invalid') {
             //invalid
             if(tag.classList.contains(hideClassName) || tag.classList.contains(showClassName)){
@@ -64,89 +122,8 @@ function testingInput(input){
             }
         }
     }
-     // valid
-    if(input.checkValidity() == true){
-        //style input
-       switchValidityStyle(input , "valid"); 
-       //style error msg input
-       let errorMsg = input.parentElement.lastElementChild;
-       switchValErrorMSg(errorMsg , "error-msg-hide" , "error-msg-show" , 'valid');
-       
-    } 
-    //invalid
-    else {
-        //style input
-        switchValidityStyle(input , "invalid");
-        //style error msg field
-        let errorMsg = input.parentElement.lastElementChild;
-        switchValErrorMSg(errorMsg , 'error-msg-hide' , "error-msg-show" , 'invalid');
 
-    }
-}
-function runTest(){
-    //valid frist selection
-    //valid from valid 
-    //valid from invalid
-    
-    //invalid frist selection
-    //invalid from invalid 
-    //invalid from valid
-
-    function setting(name , clean = true , inptVal = ''){
-        console.log(`setting : ${name} `);
-        console.log(`input val : ${inptVal}`);
-        if(clean == true){
-            //clean value
-            password.value = '';
-            //add new val
-            password.value = inptVal;
-        }
-        
-        //run testingInput 
-        testingInput(password , '8 characters , one special , one digit , one upper case')
-        console.log('result' + password.classList);
-    }
-    //valid
-    setTimeout(e=>{
-        setting('validFirstSel' , true , 'diX@ny22****' );
-    } , 5000)    
-    setTimeout(e=>{
-        setting('validFromValid' , false);
-    } , 8000)
-    setTimeout(e=>{
-        setTimeout(e=>{
-            password.value = '';
-            password.value = 'dx';
-            testingInput(password);
-        } , 1000);
-        setTimeout(e=>{
-            setting('validFromInvalid' , true , 'diX@ny22****');
-        } , 5000);
-    } , 1000);
-    // invalid
-
-    // setTimeout(e=>{
-    //     setting('invalidFirstSel' , true , 'dx' );
-    // } , 5000)    
-    // setTimeout(e=>{
-    //     setting('invalidFromInvalid' , false);
-    // } , 8000);
-    // setTimeout(e=>{
-    //     setTimeout(e=>{
-    //         password.value = '';
-    //         password.value = 'diX@ny22****';
-    //         testingInput(password);
-    //     } , 1000);
-    //     setTimeout(e=>{
-    //         setting('invalidFromValid' , true , 'dx');
-    //     } , 5000);
-    // } , 1000);
-}
+};
 
 
-
-let password = document.querySelector('#password');
-password.addEventListener('blur' , e=>{
-    testingInput(password);
-});
-
+window.addEventListener('load' , APP.setListeners());
